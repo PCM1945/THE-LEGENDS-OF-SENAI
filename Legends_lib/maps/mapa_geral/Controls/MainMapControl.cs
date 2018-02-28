67d08610b72d60ed1fb2ef70dfb0660bce7e4f90
RafaBessa <rafablou@hotmail.com>
2018-02-28 12:48:07 -0300

    Comeco da implementacao do MapControl

--- Legends_lib/maps/mapa_geral/Controls/MainMapControl.cs
+++ Legends_lib/maps/mapa_geral/Controls/MainMapControl.cs
@@ -8,30 +8,44 @@ namespace Legends_lib.maps.mapa_geral.Controls
 {
     public class MainMapControl
     {
-         private bool PodeAndar(int posX, int posY)
+        private const int ProbilidadeItem = 30;
+
+        private bool PodeAndar(int posX, int posY,Mapa mapa)
         {
-            if (1 == 1)
-                return true;
-            else
-                return false;
+            return mapa.Casa.ElementAt(posX).ElementAt(posY).Andavel;
         }//checar se a casa pode ser ocupada Na geracao
 
-         private Item GeraItem(int posX, int posY)
+
+         private Item GeraItem(int posX, int posY, Mapa mapa)
         {
-            if (PodeAndar(posX, posY))
+            if (PodeAndar(posX, posY,mapa))
                 return new Item();
             else
                 return null;
         }
 
-         public int Metrica(float tamX , float tamY, int fator = 5, float sizeCanvas = 1000) //CALCULO PARA QUANTIDADE DE QUADRADOS NO MAPA
+        private bool ProbabilidadeItem(Random rnd)
+        {
+            bool gera=false;
+            int chance = rnd.Next(0,100);
+
+            if (chance <= ProbilidadeItem)
+                gera = true;
+            
+            return gera;
+        }
+
+
+        public int Metrica(float tamX , float tamY, int fator = 5, float sizeCanvas = 1000) //CALCULO PARA QUANTIDADE DE QUADRADOS NO MAPA
         {
             return 0;
         }
 
         public Mapa InciarMapa()//iniciar o back do mapa
         {
+
             Mapa mapa = new Mapa();
+            Random rnd = new Random();//utilizar o rnd ao criar um item no mapa
             //logica de criacao do mapa
             return mapa;
         }
--- Legends_lib/maps/mapa_geral/Mapa.cs
+++ Legends_lib/maps/mapa_geral/Mapa.cs
@@ -12,8 +12,6 @@ namespace Legends_lib
         public IEnumerable<IEnumerable<Casa>> Casa { get; set; } //Item está na casa
         public int DimX { get; set; }
         public int DimY { get; set; }
-     
-
         //ADICIONAR FUNCAO PARA INICIAR AS CASAS, FAZER O MESMO NO CONTROLLER DO XAML
     }
 }
