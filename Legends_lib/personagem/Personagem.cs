323d6ef16f9eeaceaab706db9e75a62451b0c9d2
Pedro Henrique Costa <pedrohamc98@hotmail.com>
2018-03-07 09:56:06 -0300

    Personagem, Casa e Movimento

--- Legends_lib/personagem/Personagem.cs
+++ Legends_lib/personagem/Personagem.cs
@@ -7,17 +7,28 @@ namespace Legends_lib
         //criar metodo padrão de movimento
 
         //criar habilidade
-
+        public Casa casa_atual { get; set; }
         public Habilidade Habilidade { get; set; }
         public int DimXCasa { get; set; }
         public int DimYCasa { get; set; }
         public int Experiencia { get; set; }
         public int Gold { get; set; }
 
-       /* public void  Movimento(){
+        Personagem(int x, int y, int e, int g, Habilidade h)
+        {
+            this.Habilidade = h;
+            this.Gold = g;
+            this.Experiencia = e;
+            this.DimXCasa = x;
+            this.DimYCasa = y;
+            this.casa_atual = new Casa(true,x,y);
+        }
 
-
-        }*/
+       public void Mover(int xf, int yf)//f = destino
+       {
+            Casa final = new Casa(true, xf, yf);//ainda não faz a consideração de ser andável ou não
+            casa_atual = final;
+       }
 
 
     }
