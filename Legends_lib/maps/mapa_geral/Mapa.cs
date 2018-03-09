7c2acb9933810f3a1cd12a4aa06a79958618c519
RafaBessa <rafablou@hotmail.com>
2018-03-09 11:04:29 -0300

    Casas nao andaveis

--- Legends_lib/maps/mapa_geral/Mapa.cs
+++ Legends_lib/maps/mapa_geral/Mapa.cs
@@ -6,13 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Legends_lib
-{ 
+{
     public class Mapa
     {
         public Casa[][] Casa { get; set; } //Item está na casa
         public int DimX { get; set; }
         //FON
         public int DimY { get; set; }
+        
        public Mapa()
         {
             this.DimX = 20;
