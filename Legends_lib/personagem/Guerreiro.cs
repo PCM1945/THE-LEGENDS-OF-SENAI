e933d07c64161440a6c1d338da62f3623b592775
RafaBessa <rafablou@hotmail.com>
2018-03-21 11:40:23 -0300

    Recrutamento funcionando

--- Legends_lib/personagem/Guerreiro.cs
+++ Legends_lib/personagem/Guerreiro.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Legends_lib
 {
     public class Guerreiro : Personagem
     {
-
-        public Guerreiro(int corx,int cordy){
-            UrlImage = @"Assets\charters\Warrrior_spt\guerreiro.png";
+        
+        public Guerreiro(int corx,int cordy) : base() {
+            UrlImage = @"\Assets\characters\Warrrior_spt\humano\guerreiro.png";
             Custo_Gold = 200;
             Atq = 20;
             Vida = 100;
             Experiencia = 20;
             MovRange = 4;
         }
+            
+        
         
 
 
--- Legends_lib/personagem/Personagem.cs
+++ Legends_lib/personagem/Personagem.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.Collections.Generic;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Legends_lib
 {
@@ -12,20 +13,24 @@ namespace Legends_lib
         public Casa Casa_atual { get; set; }
         public Habilidade Habilidade { get; set; }
         public List<object> ItemEquipado { get; set; }
-
+        public BitmapImage bitmap;
         public int Atq { get; set; }
         public int Vida { get; set; }
 
         public int Experiencia { get; set; }
         public int Custo_Gold { get; set; }
         public int MovRange { get; set; }
-        
+        public Personagem()
+        {
+            bitmap = new BitmapImage();
+        }
 
        public void Mover(int xf, int yf)//f = destino
        {
             Casa final = new Casa(true, xf, yf);
             Casa_atual = final;
-       }
+            
+        }
         //public void PegarItem()
 
     }
