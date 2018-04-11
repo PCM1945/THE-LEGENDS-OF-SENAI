5ff9cba65ddf968cf0646e23c2857e33765118e0
DESKTOP-ICIKUVA\ads <ads@fieb.org.br>
2018-04-11 13:47:47 -0300

    JogadorControl

---
+++ Legends_lib/jogador/Controls/JogadorControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legends_lib.jogador.Controls
+{
+    public static class JogadorControl
+    {
+        private static List<Jogador> listaJogadores;
+
+        public static void AddPlayer(Jogador jogador)
+        {
+            listaJogadores.Add(jogador);
+        }
+
+        public static List<Jogador> getPlayerList()
+        {
+            return listaJogadores;
+        }
+
+        public static Jogador GetJogadorByKey(int index)
+        {
+            return listaJogadores.ElementAt(index);
+        }
+
+    }
+}
