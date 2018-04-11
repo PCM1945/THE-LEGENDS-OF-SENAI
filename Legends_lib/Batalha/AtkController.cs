7fdaaeee66a5afb74f4823d821bdb9476fcb6110
DESKTOP-ICIKUVA\ads <ads@fieb.org.br>
2018-04-11 13:24:53 -0300

    Metodo ataque. To enviando de pouquinho em pouquinho p n esquecer dnv e pq to fznd e parando

--- Legends_lib/Batalha/AtkController.cs
+++ Legends_lib/Batalha/AtkController.cs
@@ -8,5 +8,13 @@ namespace Legends_lib.Batalha
 {
     public static class AtkController
     {
+        public static int Atacar(int atq,int vida)
+        {
+            vida -= atq;
+            return vida;
+        }
+
+
+
     }
 }
