0e230dba8bbc3d8c36107ad34996ce2a9c13a465
Neilton Melgaço <neilton.melgaco@hotmail.com>
2018-04-28 14:24:12 -0300

    TesteCommit

--- Legends_lib/jogador/Controls/HabilidadeJogador.cs
+++ Legends_lib/jogador/Controls/HabilidadeJogador.cs
@@ -10,7 +10,7 @@ namespace Legends_lib
     {
         private const int MaxInvSize = 8;
 
-        public static List<Habilidade> ListaHabilidadesJogador = new List<Habilidade>()
+        public static List<Habilidade> ListaHabilidadesJogador = new List<Habilidade>() 
         {
             new Habilidade{
                 CustoGold = 100,
