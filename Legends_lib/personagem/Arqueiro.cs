7d21794974cbd8fea85284ea6aedc939e4fb29fd
Hércules Pinheiro <brbrbrkin@outlook.com>
2018-05-11 02:13:07 -0300

    Arqueiro e ganhar gold na batalha gold

---
+++ Legends_lib/personagem/Arqueiro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legends_lib
+{
+    public class Arqueiro : Personagem
+    {
+
+        public Arqueiro(int cordx, int cordy) : base()
+        {
+            UrlImage = "ms-appx:///Assets/characters/Arqueiro_spt/humano/paladino/paladino-frente.png";
+
+            Custo_Gold = 200;
+            Atq = 25;
+            VidaMax =55;
+            VidaAtual = VidaMax;
+            Mp = 30;
+            Experiencia = 20;
+            MovRange = 5;
+            AtqRange = 4;
+            MovUsados = MovRange;
+            PodeMover = false;
+            PosX = cordx;
+            PosY = cordy;
+            Nome = "Arqueiro";
+        }
+    }
+}
---
+++ Legends_lib/personagem/Hunter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legends_lib
+{
+    public class Hunter : Personagem
+    {
+
+        public Hunter(int cordx, int cordy) : base()
+        {
+            UrlImage = "ms-appx:///Assets/characters/Arqueiro_spt/esqueleto/esqueleto arqueiro/esqueleto arqueiro -frente.png";
+
+            Custo_Gold = 200;
+            Atq = 30;
+            VidaMax = 45;
+            VidaAtual = VidaMax;
+            Mp = 30;
+            Experiencia = 20;
+            MovRange = 5;
+            AtqRange = 4;
+            MovUsados = MovRange;
+            PodeMover = false;
+            PosX = cordx;
+            PosY = cordy;
+            Nome = "Hunter";
+        }
+    }
+}
